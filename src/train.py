ddd95663e8b582b685dc11c056f318785f819400
Shresth Pratap <shresthpratap17@gmail.com>
2026-06-09 22:40:58 +0530

    Got the model working

---
+++ src/train.py
@@ -0,0 +1,39 @@
+# Imports
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import joblib
+import os
+
+# Load data
+df = pd.read_csv("datasets/spam.csv", encoding="latin-1")
+
+# Split into featues and target
+X = df.iloc[:, 1]
+y = df.iloc[:, 0]
+
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1
+)
+
+# Convert text into number
+vectorizer = TfidfVectorizer(stop_words='english')
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Train the model
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# Predict on test and get the accuracy
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Save the model
+os.makedirs("models", exist_ok=True)
+joblib.dump(model, "models/model.pkl")
+print("Saved the model to models directory")
